f415742f002b6bad576f96469e62682fca567e6d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-03-22 14:02:50 +0000

    adding generate_table function

--- Dash/Dash_Templates/dropdown_filter_table.py
+++ Dash/Dash_Templates/dropdown_filter_table.py
@@ -8,3 +8,14 @@ from sklearn.utils import resample
 ## Reading data 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 df = df[['continent', 'country', 'pop', 'lifeExp']]  # prune columns for example
+
+def generate_table(dataframe, max_rows = 10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))] 
+    )
